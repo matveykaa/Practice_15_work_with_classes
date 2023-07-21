c327306aadab54f5a3277c1c757ab2e13e95351c
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-21 14:37:09 +0300

    Final commit

--- nikola.py
+++ nikola.py
@@ -1,17 +1,26 @@
 class Nikola():
     __slots__ = ['name', 'age']
     def __init__(self, param1: str,  param2: int):
-        self.name = param1
+        self.name = self.check(param1)
         self.age = param2
-
-
-
-
-
-
+    def check(self, name):
+        if name.capitalize() not in ("Николай", "Nikola"):
+            print(f'Я не {name}, я Николай')
+        else:
+            print('Успешно!')
+        return name
 
 if __name__ == '__main__':
-    input_name = input('Name - ')
-    man = Nikola()
+    while True:
+        input_name = input('Имя - ')
+        if input_name == '':
+            raise KeyboardInterrupt
+        input_age = input('Возраст - ')
+        if input_age.isnumeric():
+            input_age = int(input_age)
+            Nikola(input_name, input_age)
+        else:
+            print('- - Возраст должен быть целым числом - -')
+
 
 
