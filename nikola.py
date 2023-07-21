2d42686a78740b4d4e37a8266a669b8b20433f64
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-21 14:17:57 +0300

    Init commit

---
+++ nikola.py
@@ -0,0 +1,17 @@
+class Nikola():
+    __slots__ = ['name', 'age']
+    def __init__(self, param1: str,  param2: int):
+        self.name = param1
+        self.age = param2
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    input_name = input('Name - ')
+    man = Nikola()
+
+
